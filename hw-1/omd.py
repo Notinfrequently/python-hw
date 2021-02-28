ad2d9d88974e09768d3887a68848ed3d19ed0e3e
Ed <33519284+Notinfrequently@users.noreply.github.com>
2021-02-28 21:42:37 +0300

    Update omd.py

--- hw-1/omd.py
+++ hw-1/omd.py
@@ -1,47 +1,78 @@
-def step2_no_umbrella():
-    print('''
-              ,    ,   ,
-            ,   ,    ,   ,
+def show_them_(it): print(it)
+
+duck =  '''
                __
-             _/__\_
              <(o )___
               ( ._> /
-               `---'
-    ''')
+               `---' 
+'''
+options = {'да': True, 'нет': False}
+
+questions = {
+    "puddles" : "Is there puddles?",
+    "cold" : "Is it cold outside?",
+    "shining_sun" : "Is it sunny outside? ",
+    "wind" : "Is it windy outside?",
+    "rain" : "Is it raining outside?"
+}
+
+def question_of_(function):
+    return questions[function.__name__]
+
+def puddles(yes):
+    '''Is there puddles?
+    '''
+    if yes:
+        print("Some waterproof boots?\n")
+    else:
+        print("It hasn't been raining for a long time.\n")
+
+def cold(yes):
+    '''Is it cold outside?
+    '''
+    if yes:
+        print("Warm jacket it is!\n")
+    else:
+        print("Duck do not fear cold!.\n")
+
+def shining_sun(yes):
+    '''Is it sunny outside? 
+    '''
+    if yes:
+        print("Do ducks need a sunglases?\n")
+    else:
+        print("Grumpy weather, ay?\n")
+
+def wind(yes):
+    '''Is it windy outside?
+    '''
+    if yes:
+        print("Maybe duck do need a jacket.\n")
+    else:
+        print("That's nice!\n")
 
 
-def step2_umbrella():
-    print('''
-              ,    ,   ,
-            ,   ,    ,   ,
-               _.-^-.
-             _/'"'|`"`
-             <(o )|__
-              ( ._j /
-               `---'
-    ''')
+def rain(yes):
+    '''Is it raining outside?
+    '''
+    if yes:
+        print("Better take an umbrella!\n")
+    else:
+        print("No need in umbrella.\n")
 
 
-def step1():
-    print(
-        '''
-        Утка-маляр
-               __
-             _/__\_
-             <(o )___
-              ( ._> /
-               `---'     .-^-.
-        решила погулять.'"'|`"`
-        Взять ей зонтик?   j
-        '''
-    )
-
+def ask_duck_about_(function):
+    show_them_(question_of_(function))
     option = ''
-    options = {'да': step2_umbrella, 'нет': step2_no_umbrella}
+    options = {'yes' : True, 'no' : False}
     while option not in options:
         option = input('Выберите: ({}/{}): '.format(*options))
-    return options[option]()
+    return function(options[option])
 
-
-if __name__ == '__main__':
-    step1()
+if __name__ == "__main__":
+    show_them_(duck)
+    ask_duck_about_(rain)
+    ask_duck_about_(puddles)
+    ask_duck_about_(wind)
+    ask_duck_about_(shining_sun)
+    ask_duck_about_(cold)
