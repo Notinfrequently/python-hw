4316776b9bad2259d1eb6822417d013a0487e3ad
Ed Edsons <hard2explain@yandex.ru>
2021-05-24 17:39:01 +0300

    tahts was tough

---
+++ hw_6/README.md
@@ -0,0 +1,6 @@
+# Инструкции для запуска тестов
+
+
+Для запуска тестов через pytest - `python3 -m pytest -v test_funcs.py`
+
+Для запуска тестов через doctest - `python3 -m doctest -v funcs.py`
--- hw_7/decorators.py
+++ hw_7/decorators.py
@@ -1,6 +1,7 @@
 from time import sleep, time
 from random import random
 import string
+import re
 
 
 def calc_duration(func):
@@ -8,13 +9,13 @@ def calc_duration(func):
         start_time = time()
         result_func = func(*args, **kwargs)
         end_time = time()
-        print(f"Executing took: {time_to_finish-time_to_start}.")
+        print(f"Executing took: {end_time-start_time}.")
         return result_func
     return decorated
 
 
 @calc_duration
-def long_executing_task():  # func(*args, **kwargs) ==== long_executing_task
+def long_executing_task():
     for index in range(3):
         print(f'Iteration {index + 1}')
         sleep(random())
@@ -26,7 +27,7 @@ def suppress_errors(errors):
             try:
                 return func(*args, **kwargs)
             except errors as e:
-                print(f"error: {e}")
+                print(f"Problem with: {e}")
         return decorated
     return decorator
 
@@ -40,15 +41,15 @@ def potentially_unsafe_func(key: str):
     data = {'name': 'test', 'age': 30}
     return data[key]
 
+print(potentially_unsafe_func('name'))  # everything is ok
+print(potentially_unsafe_func('last_name')) 
 
 def result_between(value_min, value_max):
     def decorator(func):
         def decorated(*args, **kwargs):
             result = func(*args, **kwargs)
-            if value_min <= result <= value_max:
-                return result
-            else:
-                return ValueError
+            if not (value_min <= result <= value_max):
+                raise ValueError                
         return decorated
     return decorator
 
@@ -57,10 +58,8 @@ def len_more_than(s_len):
     def decorator(func):
         def decorated(*args, **kwargs):
             result_func = func(*args, **kwargs)
-            if len(result_func) >= s_len:
-                return result_func
-            else:
-                return ValueError
+            if (len(result_func) < s_len):
+                raise ValueError
         return decorated
     return decorator
 
@@ -70,63 +69,53 @@ def sum_of_values(numbers):
     return sum(numbers)
 
 
-@len_more_than(100)
+@len_more_than(10)
 def show_message(message: str) -> str:
-    return f'Hi, you sent: {message}'
+    return f'{message}'
 
+#sum_of_values((1, 3))  # ValueError
+#show_message('123456789')  # ValueError
 
 def replace_commas(func):
     def decorated(*args, **kwargs):
         result_func = func(*args, **kwargs)
-        for i in list(string.punctuation):
-            result_func = result_func.replace(i, ' ')
-        return result_func
+        return re.sub(r'[^\s\w]', " ", result_func)
     return decorated
-
 
 def words_title(func):
     def decorated(*args, **kwargs):
         result_func = func(*args, **kwargs)
-        whitespace_positions = []
-        n = 0
-        for i in result_func:
-            if i == ' ':
-                whitespace_positions.append(n)
-            n += 1
-        list_result = list(result_func)
-        for i in range(whitespace_positions.__len__()):
-            if i == 0:
-                list_result[whitespace_positions[0] + 1] = str(list_result[whitespace_positions[0] + 1]).upper()
-            elif i == whitespace_positions.__len__() - 1:
-                list_result[whitespace_positions[whitespace_positions.__len__() - 1] - 1] = \
-                    str(list_result[whitespace_positions[whitespace_positions.__len__() - 1] - 1]).upper()
-            else:
-                list_result[whitespace_positions[i] - 1] = str(list_result[whitespace_positions[i] - 1]).upper()
-                list_result[whitespace_positions[i] + 1] = str(list_result[whitespace_positions[i] + 1]).upper()
-        return ''.join(list_result)
+        def AaA(s):
+            s = s.group(1)
+            if len(s) == 1:
+                return " " + s
+            return " " + s[0].upper() + s[1:len(s) - 1] + s[-1].upper()
+        return (re.sub(r"(?:\s+)([A-Za-z0-9]+)(?=\s+)", AaA, result_func)).strip()
+        #return " ".join(list(map(AaA, result_func.split())))
     return decorated
-
 
 @words_title
 @replace_commas
 def process_text(text: str) -> str:
     return text.replace(':', ',')
 
-
 @replace_commas
 @words_title
 def another_process(text: str) -> str:
     return text.replace(':', ',')
 
+print(process_text('the French revolution resulted in 3 concepts: freedom,equality,fraternity')) 
+print(another_process('the French revolution resulted in 3 concepts: freedom,equality,fraternity'))
+
 
 def cache_result():
-    _cache_result = {}
+    _cache = {}
 
     def decorator(func):
         def decorated(*args, **kwargs):
             if args not in _cache_result:
-                _cache_result[args] = func(*args, **kwargs)
-            return _cache_result[args]
+                _cache[args] = func(*args, **kwargs)
+            return _cache[args]
         return decorated
     return decorator
 
